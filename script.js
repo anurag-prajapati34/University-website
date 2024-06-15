4074c8d484bbe075ddb5759730821b462fb766ef
anuragtech34 <prajapatianurag73240@gmail.com>
2024-06-15 12:26:59 +0530

    Home page Header section completer

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let menu=document.querySelector(".nav-links");
+
+function hideMenu(){
+ menu.style.display="none";
+ menu.style.transition=`0.7s ease`
+
+}
+
+function showMenu(){
+    menu.style.display="block";
+}
